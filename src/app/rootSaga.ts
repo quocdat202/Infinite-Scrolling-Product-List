69ccfb8b7f61c780199b4d06247a7bd243da99fe
quocdat202 <quocdat732@gmail.com>
2024-01-27 11:41:18 +0700

    upload product

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,10 +1,8 @@
 import { all } from 'redux-saga/effects';
-import { introduceSaga } from 'sagas/introduceSaga';
 import { productsSaga } from 'sagas/productsSaga';
 
 export default function* rootSaga() {
     yield all([
-        introduceSaga(),
         productsSaga()
     ])
 }
